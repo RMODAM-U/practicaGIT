1f36fd8f9cc4473de0a589fa7cf4e5492a0f05ca
Roberto Martínez Onofre <roberto_marti7@hotmail.com>
2024-12-14 11:38:31 +0100

    Actualizados precios de telegramas ordinarios y urgentes

--- ejercicioTelegrama/ejercicioTelegrama/Form1.cs
+++ ejercicioTelegrama/ejercicioTelegrama/Form1.cs
@@ -50,23 +50,23 @@ namespace ejercicioTelegrama
             if (tipoTelegrama == 'o')
             {
                 if (numPalabras <= 10)
-                {
-                    coste = 2.5;
+                {   
+                    coste = 3;
                 }
                 else
                 {
-                    coste = 2.5 + 0.5 * (numPalabras - 10);
+                    coste = 3 + 0.5 * (numPalabras - 10);
                 }
             }
             else // tipoTelegrama == 'u'
             {
                 if (numPalabras <= 10)
                 {
-                    coste = 5;
+                    coste = 6;
                 }
                 else
                 {
-                    coste = 5 + 0.75 * (numPalabras - 10);
+                    coste = 6 + 0.75 * (numPalabras - 10);
                 }
             }
 
